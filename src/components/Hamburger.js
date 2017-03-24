f2ff888b74dfb153f129e7f86125903a1c14145d
Glendale Acosta <gglendale17@gmail.com>
2017-03-23 20:04:04 -0700

    Animated Hamburger.

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Motion, spring } from 'react-motion';
 
-const Hamburger = () => (
-  <div className="menu">
-    <p>MENU</p>
-    <div className="hamburger">
-      <div className="hamburger-mid" />
-    </div>
-  </div>
-);
+export default class Hamburger extends React.Component {
 
-export default Hamburger;
+  renderHamburger = (interpolatingStyles) => {
+    const styles = {
+      WebkitTransform: `translateY(${interpolatingStyles.y}%)`,
+      transform: `translateY(${interpolatingStyles.y}%)`,
+    };
+    return (
+      <div style={styles} className="menu">
+        <p>MENU</p>
+        <div className="hamburger">
+          <div className="hamburger-mid" />
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <Motion defaultStyle={{ y: -300 }} style={{ y: spring(0, { stiffness: 320, damping: 60 }) }} >
+        {this.renderHamburger}
+      </Motion>
+    );
+  }
+}
