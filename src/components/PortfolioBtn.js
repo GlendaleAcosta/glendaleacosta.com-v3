6d7323932f5da502e714d6bf74c65f9aa2b15061
Glendale Acosta <gglendale17@gmail.com>
2017-03-23 23:16:44 -0700

    Added portfolio button hover animation.

--- src/components/PortfolioBtn.js
+++ src/components/PortfolioBtn.js
@@ -4,6 +4,25 @@ import { Link } from 'react-router';
 
 export default class PortfolioBtn extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: spring(-105, { stiffness: 35, damping: 16 })
+    };
+  }
+
+  onEnter = () => {
+    this.setState({
+      test: spring(0, { stiffness: 80, damping: 16 })
+    });
+  }
+
+  onLeave = () => {
+    this.setState({
+      test: spring(-105, { stiffness: 80, damping: 16 })
+    });
+  }
+
   renderBtn = (interpolatingStyles) => {
     const styles = {
       WebkitTransform: `translateY(${interpolatingStyles.y}%)`,
@@ -28,11 +47,11 @@ export default class PortfolioBtn extends Component {
 
   render() {
     return (
-      <div className="portfolio-btn-container">
+      <div onMouseEnter={this.onEnter} onMouseLeave={this.onLeave} className="portfolio-btn-container">
         <Motion defaultStyle={{ y: 300 }} style={{ y: spring(0, { stiffness: 320, damping: 60 }) }} >
           {this.renderBtn}
         </Motion>
-        <Motion defaultStyle={{ y: 200 }} style={{ y: spring(-100, { stiffness: 35, damping: 13 }) }} >
+        <Motion defaultStyle={{ y: 200 }} style={{ y: this.state.test }} >
           {this.renderBg}
         </Motion>
       </div>
